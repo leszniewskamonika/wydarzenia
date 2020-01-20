78b895c4f7708250c1d82a1830d3fab664a5bd44
Leszniewska Monika <monia.leszniewska@gmail.com>
2020-01-20 01:09:30 +0100

    Niedziałające klasy :(

--- PHP/testy_strony.php
+++ PHP/testy_strony.php
@@ -9,6 +9,7 @@ require_once ('Class\ToGo.php');
 require_once ('Class\Filter.php');
 require_once ('Class\ImgUpload.php');
 
+
 class Test extends TestCase
 {
 
@@ -19,7 +20,7 @@ class Test extends TestCase
         $this->assertEquals(1,$user->getId());
 
     }
-
+//niedziałający test
     public function testSecondTest()
     {
 
@@ -29,4 +30,21 @@ class Test extends TestCase
 
     }
 
+    public function testThirdTest() //klasa User posiada atrybut _haslo
+    {
+        $this -> assertClassHasAttribute ( '_haslo' ,User:: class );
+    }
+
+//niedziałający test ponieważ zła metoda :(
+    public function testUser()
+    {
+        // kopiujemy naszą klasę User
+        $stub = $this->getMockBuilder('User');
+        // zmieniamy działanie jednej z metod
+        $stub->addyMethods('doLogin')->willReturn(true);
+        // test niepoprawnego adresu e-mail
+        $this->assertFalse($stub->User('username', 'this-is-not-an-email'));
+        // test dobrego adresu e-mail
+        $this->assertTrue($stub->User('username', 'valid@e.mail'));
+    }
 }
